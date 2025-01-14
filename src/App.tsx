18a5437a3980b529fb175b9ce9fa488d62154f08
vihanpereraux <vihanpereraux@gmail.com>
2025-01-14 06:02:18 +0000

    add thumbnails structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='wrapper'>
+        <div className='video'>
+          {/* poster */}
+          <img src="/posters/poster_1.jpg" alt="Poster 1" />
+        </div>
+        <div className='video'>
+          {/* poster */}
+          <img src="/posters/poster_2.jpg" alt="Poster 2" />
+        </div>
+        <div className='video'>
+          {/* poster */}
+          <img src="/posters/poster_3.jpg" alt="Poster 3" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
